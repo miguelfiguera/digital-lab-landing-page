068bb2be8f1ade5393d278b2182a6a4c2952126d
Miguel Figuera <36214292+miguelfiguera@users.noreply.github.com>
2024-06-09 20:59:39 -0400

    add toggle theme button to navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+'use client';
+import { useState } from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -6,22 +8,29 @@ import Navbar from "@/components/generalUse/Navbar";
 import Footer from "@/components/generalUse/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
+/* export const metadata: Metadata = {
   title: "Digital Lab",
   description: "Agencia de Marketing Digital, Diseño y Desarrollo Web.",
 };
-
+ */
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+const [theme,setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
   return (
-    <html lang="en">
+    <html lang="en" className={`${theme}`} >
       <body className={inter.className}>
         <Toaster />
 
-        <Navbar />
+        <Navbar setTheme={toggleTheme} theme={theme} />
 
         {children}
 
--- components/generalUse/Navbar.tsx
+++ components/generalUse/Navbar.tsx
@@ -20,7 +20,55 @@ function MountainIcon(props: any) {
   );
 }
 
-export default function Navbar() {
+function MoonIcon(props:any) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z" />
+    </svg>
+  )
+}
+
+
+
+function SunIcon(props:any) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <circle cx="12" cy="12" r="4" />
+      <path d="M12 2v2" />
+      <path d="M12 20v2" />
+      <path d="m4.93 4.93 1.41 1.41" />
+      <path d="m17.66 17.66 1.41 1.41" />
+      <path d="M2 12h2" />
+      <path d="M20 12h2" />
+      <path d="m6.34 17.66-1.41 1.41" />
+      <path d="m19.07 4.93-1.41 1.41" />
+    </svg>
+  )
+}
+
+export default function Navbar({setTheme,theme}:{setTheme:()=>void,theme:string}) {
   return (
     <header className="px-4 lg:px-6 h-14 flex items-center fixed top-0 left-0 right-0 z-50 bg-white dark:bg-gray-950 shadow">
       <Link
@@ -60,6 +108,13 @@ export default function Navbar() {
         >
           Contact
         </Link>
+        <button>
+
+
+          {theme === 'dark' ? <SunIcon className="h-6 w-6" onClick={() => setTheme('light')} /> : <MoonIcon className="h-6 w-6" onClick={() => setTheme('dark')} />}
+
+
+        </button>
       </nav>
     </header>
   );
